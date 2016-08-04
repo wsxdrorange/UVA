335b89af219c8ee82c41a494eb03ae5779df5cec
Rick Zhang <rick.zhang@yahoo.com>
2016-08-04 16:40:28 -0500

    Create 494-KindergardenCountingGame.cpp

---
+++ Cpp/494-KindergardenCountingGame.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	char c;
+	bool flag = false; // remember the first character of a word if letter
+	int words = 0;
+
+	c = cin.get();
+	while (!cin.eof()){	// untile the end of the file is reached
+
+		while (c != '\n'){ // for per line
+			if (( c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
+				if (!flag){
+					// if false
+					flag = true;
+					words++;
+				}
+			}
+			else
+				flag = false;
+
+			c = cin.get();
+		}
+
+		cout << words << endl;
+		words = 0;
+		flag = false;
+		c = cin.get();
+	}
+
+	return 0;
+}
